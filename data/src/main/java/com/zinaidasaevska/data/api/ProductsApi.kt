72906ac540de3ecbebb9a6e4b866a2f7335310ee
ZinaidaSaevska <zinaidasaevska@gmail.com>
2025-01-08 16:39:03 +0200

    Create Repository

--- data/src/main/java/com/zinaidasaevska/data/api/ProductsApi.kt
+++ data/src/main/java/com/zinaidasaevska/data/api/ProductsApi.kt
@@ -5,7 +5,7 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-interface SearchProductsApi {
+interface ProductsApi {
 
     @GET("search")
     suspend fun searchProducts(@Query("q")query: String): Response<SearchResponse>
