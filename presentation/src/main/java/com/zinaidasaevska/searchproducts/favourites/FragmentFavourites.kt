d6e2510a7a4ec2a66e182c037f1881b012710394
ZinaidaSaevska <zinaidasaevska@gmail.com>
2025-01-12 18:12:03 +0200

    Add navigation to FragmentProductDetails from FragmentSearch and FragmentFavourites

--- presentation/src/main/java/com/zinaidasaevska/searchproducts/favourites/FragmentFavourites.kt
+++ presentation/src/main/java/com/zinaidasaevska/searchproducts/favourites/FragmentFavourites.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import androidx.compose.material.MaterialTheme
 import androidx.compose.ui.platform.ComposeView
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.zinaidasaevska.domain.model.Product
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class FragmentFavourites : Fragment() {
@@ -23,10 +25,19 @@ class FragmentFavourites : Fragment() {
                 MaterialTheme {
                     FavouritesList(
                         viewModel = viewModel,
-                        onFavouriteIconClick = viewModel::removeFromFavourites
+                        onFavouriteIconClick = viewModel::removeFromFavourites,
+                        onItemClick = ::navigateToProductDetails
                     )
                 }
             }
         }
+    }
+
+    private fun navigateToProductDetails(product: Product) {
+        findNavController().navigate(
+            FragmentFavouritesDirections.actionFragmentFavouritesToFragmentProductDetails(
+                product
+            )
+        )
     }
 }
--- presentation/src/main/java/com/zinaidasaevska/searchproducts/search/FragmentSearch.kt
+++ presentation/src/main/java/com/zinaidasaevska/searchproducts/search/FragmentSearch.kt
@@ -2,13 +2,12 @@ package com.zinaidasaevska.searchproducts.search
 
 import android.content.res.Configuration
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.GridLayoutManager
 import com.zinaidasaevska.domain.model.Product
 import com.zinaidasaevska.searchproducts.databinding.FragmentSearchBinding
@@ -93,5 +92,13 @@ class FragmentSearch : Fragment(), AdapterSearch.IProductFavouriteListener {
 
     override fun removeProductFromFavourites(productId: Int) {
         viewModel.removeFromFavourites(productId)
+    }
+
+    override fun onItemClick(product: Product) {
+        findNavController().navigate(
+            FragmentSearchDirections.actionFragmentSearchToFragmentProductDetails(
+                product
+            )
+        )
     }
 }
