b662d460f70584b2ea720b7ac60cf1f17493229c
ZinaidaSaevska <zinaidasaevska@gmail.com>
2025-01-12 11:37:04 +0200

    Add option to navigate to Favourites screen from the toolbar

--- presentation/src/main/java/com/zinaidasaevska/searchproducts/MainActivity.kt
+++ presentation/src/main/java/com/zinaidasaevska/searchproducts/MainActivity.kt
@@ -1,19 +1,39 @@
 package com.zinaidasaevska.searchproducts
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import com.zinaidasaevska.searchproducts.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private val navController: NavController by lazy { findNavController(R.id.fragment_container_view) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when {
+            item.itemId == R.id.favourites -> {
+                navController.navigate(R.id.fragmentFavourites)
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
